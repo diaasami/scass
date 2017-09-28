7c7a7d46d81a2a1ce632d70e8c30a7b264e11f66
Diaa Sami <diaasami@fastmail.fm>
2017-09-28 23:00:26 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
 #include "objparser.h"
+#include "objmodel.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    ObjModel model;
+    model.load("cube.obj");
+
     return 0;
 }
